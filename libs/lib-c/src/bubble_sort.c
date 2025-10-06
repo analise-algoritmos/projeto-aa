fe7332fb80a79621d6a784b1a9d482e63dccdaad
Aline Milene <alinem2104@gmail.com>
2025-10-05 21:30:05 -0300

    feat: creating a C library draft

---
+++ libs/lib-c/src/bubble_sort.c
@@ -0,0 +1,16 @@
+#include "bubble_sort.h"
+
+void bubble_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int swapped = 0;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+        if (!swapped) break;
+    }
+}
